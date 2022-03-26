e81306a7e7fdf36572635a53678574f371cc811b
Neelporos <sawkotalimahmud@gmail.com>
2022-03-26 08:17:11 -0700

    Projects Almost Done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ function App() {
   return (
     <div>
       <Services></Services>
+      <h2>How React Work</h2>
+      <p>mostly react work by app function with jsx. and reactDom.render method. and component is most important part of react. its work like body part of a react website or building blocks. and most easy part of a component we can build so many nested  component under app component by jsx. JSX is a syntex like HTML. but there have difference between JSX and HTML.</p>
+      <h2>How UseState Work</h2>
+      <p>UseState is a most important and useful function. and its a hook for use value to set in a function or show value on ui. we can use UseState in so many time like a object, a array, a boolean, in a class. its mostly useful on local component.  </p>
     </div>
   );
 }
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,40 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Cart.css'
 
 const Cart = ({ cart }) => {
-
-
-    const getOneServicebtn = () => {
-        let index = 0;
-        if (cart.length > 0) {
-            const min = 0;
-            const max = cart.length - 1;
-            let index2 = min + Math.random() * (max - min);
-            index = Math.floor(Math.random() * (max - min + 1)) + min;
-        }
-
-
-    }
-
-    const clearCart = (cart) => {
-        while (cart.length > 0) {
-            cart.pop();
-        }
-    }
 
     let serviceName = " "
     for (const service of cart) {
         serviceName = serviceName + service.name;
         serviceName = serviceName + "\n"
-
     }
+
+
+
 
     return (
         <div className='cart'>
             <h3>Get Your Service</h3>
             <p>{cart.length}</p>
-            <p>{serviceName}{ }</p>
-            <button onClick={getOneServicebtn}>Get One Service</button>
+            <p>{serviceName}</p>
+            <p></p>
+            <button>Get One Service</button>
             <button>Clear Cart</button>
         </div>
     );
